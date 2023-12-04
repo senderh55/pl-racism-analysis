c47992c1b58fa86d111178fabf5ef6c971ba47d1
Sender Hodik <senderh55@gmail.com>
2023-12-04 21:43:13 +0200

    tests updated

--- backend/tests/nlp.service.test.ts
+++ backend/tests/nlp.service.test.ts
@@ -1,24 +1,51 @@
-// FILEPATH: /c:/Users/sende/Desktop/projects/PL-Racism-Analysis/backend/src/services/nlp.service.test.ts
+// File: src/__tests__/nlp.service.test.ts
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
 import { analyzeFetchedArticles } from "../src/services/nlp.service";
-import { fetchNewsArticles } from "../src/services/news.service";
+import * as newsService from "../src/services/news.service";
+import { Event } from "../src/models/event.model";
 
-// Define mock functions
-const mockFindOne = jest.fn();
-mockFindOne.mockResolvedValue({});
-const mockSave = jest.fn();
-
+// Mock the newsService
 jest.mock("../src/services/news.service");
+type MockedFetchNewsArticles = jest.Mock<
+  ReturnType<typeof newsService.fetchNewsArticles>,
+  Parameters<typeof newsService.fetchNewsArticles>
+>;
+// Define the type for the mocked fetchNewsArticles function
+// Define the type for the mocked Event model
+type MockedEventModel = {
+  findOne: jest.Mock<any, any>;
+  save: jest.Mock<any, any>;
+  // Other mock methods or properties as needed
+};
 
-// Mock the Event model
-jest.doMock("../src/models/event.model", () => {
+jest.mock("../src/models/event.model", () => {
+  const mockEventModel: MockedEventModel = {
+    findOne: jest.fn(),
+    save: jest.fn(),
+    // Other mock methods or properties as needed
+  };
+
   return {
-    Event: function () {
-      return { save: mockSave };
-    },
-    findOne: mockFindOne,
+    Event: mockEventModel,
   };
 });
 
+let mongoServer: MongoMemoryServer;
+
+// Setup and teardown of the in-memory MongoDB instance
+beforeAll(async () => {
+  mongoServer = await MongoMemoryServer.create();
+  const mongoUri = mongoServer.getUri();
+  await mongoose.connect(mongoUri, {});
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+  await mongoServer.stop();
+});
+
+// Test suite for analyzeFetchedArticles
 describe("analyzeFetchedArticles", () => {
   beforeEach(() => {
     jest.clearAllMocks();
@@ -34,19 +61,23 @@ describe("analyzeFetchedArticles", () => {
         publishedAt: "2023-11-11T00:00:00Z",
       },
     ];
-    (fetchNewsArticles as jest.Mock).mockResolvedValue(articles);
-    mockFindOne.mockResolvedValue(null);
+
+    (
+      newsService.fetchNewsArticles as MockedFetchNewsArticles
+    ).mockResolvedValue(articles);
+
+    (Event.findOne as jest.Mock).mockResolvedValue(null);
 
     await analyzeFetchedArticles("racism", "2023-11-07", "2023-12-10");
 
-    expect(fetchNewsArticles).toHaveBeenCalledWith(
+    expect(newsService.fetchNewsArticles).toHaveBeenCalledWith(
       "racism",
       "2023-11-07",
       "2023-12-10"
     );
-    expect(mockFindOne).toHaveBeenCalledWith({ url: articles[0].url });
-    expect(mockSave).toHaveBeenCalled();
-  }, 5000); // Increased timeout
+    expect(Event.findOne).toHaveBeenCalledWith({ url: articles[0].url });
+    expect(new Event().save).toHaveBeenCalled();
+  });
 
   it("should not save an article if it already exists in the database", async () => {
     const articles = [
@@ -58,18 +89,27 @@ describe("analyzeFetchedArticles", () => {
         publishedAt: "2023-11-11T00:00:00Z",
       },
     ];
-    (fetchNewsArticles as jest.Mock).mockResolvedValue(articles);
-    mockFindOne.mockResolvedValue(articles[0]);
+
+    (
+      newsService.fetchNewsArticles as MockedFetchNewsArticles
+    ).mockResolvedValue(articles);
+
+    const { Event } = require("../src/models/event.model");
+    Event.findOne.mockResolvedValue(articles[0]);
 
     await analyzeFetchedArticles("racism", "2023-11-07", "2023-12-10");
 
-    expect(mockFindOne).toHaveBeenCalledWith({ url: articles[0].url });
-    expect(mockSave).not.toHaveBeenCalled();
-  }, 5000); // Increased timeout
+    expect(Event.findOne).toHaveBeenCalledWith({ url: articles[0].url });
+    expect(new Event().save).not.toHaveBeenCalled();
+  });
 
   it("should handle errors during article analysis", async () => {
     const error = new Error("Test error");
-    (fetchNewsArticles as jest.Mock).mockRejectedValue(error);
+
+    (
+      newsService.fetchNewsArticles as MockedFetchNewsArticles
+    ).mockRejectedValue(error);
+
     const consoleErrorMock = jest.spyOn(console, "error").mockImplementation();
 
     await expect(
